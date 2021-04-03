2ab500ecdf04ae69d2ddd367f9e12105b7b7ef61
asdfjkl <user@domain.invalid>
2021-04-03 16:53:17 +0200

    better debouncing of switches

--- eboard.ino
+++ eboard.ino
@@ -37,14 +37,26 @@ int RANKS[8] = {RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8};
 // holds the current state of the buttons and records
 // if a button was pressed
 bool STATUS[8][8] = {
-    {false, false, false, false, false, false, false, false},
-    {false, false, false, false, false, false, false, false},
-    {false, false, false, false, false, false, false, false},
-    {false, false, false, false, false, false, false, false},
-    {false, false, false, false, false, false, false, false},
-    {false, false, false, false, false, false, false, false},
-    {false, false, false, false, false, false, false, false},
-    {false, false, false, false, false, false, false, false}};
+  {false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false}
+};
+
+int DEBOUNCE_TIMER[8][8] = {
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}
+};
 
 
 void setup()
@@ -52,16 +64,15 @@ void setup()
   // put your setup code here, to run once:
 
   /*
-  Approach that was used in Berger's Sish & Solus chess board. For reed-switches, it requires that the diodes point 
-  correctly (cf. wiring diagram). For tacticle switches, this does not matter.
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  - Connect the rows to 8 pins of the microcontroller, setting them as OUTPUT, and initializing them all to '1' (High).
-  - Connect the columns to 8 pins of the microcontroller, setting them all as INPUT with pull-up resistor enabled.
-  - Set to '0' (Low) one row and read the value of all columns. Do the same with the other seven rows, one by one.
-  - In every full sampling of the board (of the 8 rows), we obtain a matrix of 8 x 8 bits.
-  - Detecting changes that occur in the matrix at every sampling, we can know what piece was moved.
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*/
+    Approach that was used in Berger's Solus chess (but here for tactile switches)
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    - Connect the rows to 8 pins of the microcontroller, setting them as OUTPUT, and initializing them all to '1' (High).
+    - Connect the columns to 8 pins of the microcontroller, setting them all as INPUT with pull-up resistor enabled.
+    - Set to '0' (Low) one row and read the value of all columns. Do the same with the other seven rows, one by one.
+    - In every full sampling of the board (of the 8 rows), we obtain a matrix of 8 x 8 bits.
+    - Detecting changes that occur in the matrix at every sampling, we can know what piece was moved.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  */
   pinMode(RANK_1, OUTPUT);
   pinMode(RANK_2, OUTPUT);
   pinMode(RANK_3, OUTPUT);
@@ -92,10 +103,6 @@ int reset_timer = 0;
 
 void loop()
 {
-  delay(100);
-
-  reset_timer += 100;
-
   for (int i = 0; i < 8; i++)
   {
     int rank_i = RANKS[i];
@@ -105,52 +112,43 @@ void loop()
       int file_j = FILES[j];
       int val = digitalRead(file_j);
       if (val == HIGH)
-      {        
+      {
+        // val == HIGH: field is not pressed
+        if (STATUS[i][j]) {
+          // active, check if we are below threshold
+          if (DEBOUNCE_TIMER[i][j] - 1 < 2) {
+            STATUS[i][j] = false;
+          }
+        }
+        if (DEBOUNCE_TIMER[i][j] > 0) {
+          DEBOUNCE_TIMER[i][j] = DEBOUNCE_TIMER[i][j] - 1;
+        }
       }
       if (val == LOW)
       {
-        // val == LOW: field is occupied
-        // but we need to make sure that
-        // we send the field info only once
-        // even if the user presses the button
-        // for a longer time period        
-        if(!STATUS[i][j]) {
-          // button was _not_ pressed before
-          // first reset all other states
-          for( int k=0;k<8;k++) {
-            for( int l=0;l<8;l++) {
-              STATUS[k][l] = false;
-            }
-          }
-          // set button status
-          STATUS[i][j] = true;
-          char s[] = "a1\n";
-          s[0] = 97 + j;
-          s[1] = 49 + i;
-
-          reset_timer = 0;
-          
-          #ifdef USB_KEYBOARD
+        // val == LOW: field is pressed
+        // val == HIGH: field is not pressed
+        if (!STATUS[i][j]) {
+          // not active, check if we are above threshold
+          if (DEBOUNCE_TIMER[i][j] + 1 > 6) {
+            STATUS[i][j] = true;
+            char s[] = "a1\n";
+            s[0] = 97 + j;
+            s[1] = 49 + i;
+#ifdef USB_KEYBOARD
             for (int m = 0; m < 2; m++)
             {
               Keyboard.write(s[m]);
             }
-          #endif
-          #ifdef BOARD_DEBUG
-            Serial.println("button press");    
-            Serial.println(s);    
-          #endif          
-        } else {
-          // if STATUS[i][j] is true, we already sent the command once previously
-          // we reset this after some time (1 second), otherwise we can't press one field two
-          // times consecutively
-          if(reset_timer > 1000) {
-            for( int k=0;k<8;k++) {
-              for( int l=0;l<8;l++) {
-                STATUS[k][l] = false;
-              }
-            }
+#endif
+#ifdef BOARD_DEBUG
+            Serial.println("button press");
+            Serial.println(s);
+#endif
           }
+        }
+        if (DEBOUNCE_TIMER[i][j] + 1 < 9) {
+          DEBOUNCE_TIMER[i][j] = DEBOUNCE_TIMER[i][j] + 1;
         }
       }
     }
